7e7a1e010ec1ff8ad9f07989e86c1dac3022802d
Ethan Epp <ethan3048@yahoo.com>
2025-02-28 09:54:43 -0800

    fixed paths

--- examples/capture_activations.py
+++ examples/capture_activations.py
@@ -8,6 +8,9 @@
 
 # Imports
 import os
+import sys
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import platform
 import collections
 import time
